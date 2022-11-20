7c1d81c54a8031dc2fd6170479725765d9c06fcf
unknown <franciscobossio10@gmail.com>
2022-11-20 16:13:08 -0300

    Sube TP

---
+++ Funciones.h
@@ -0,0 +1,328 @@
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <fstream>
+#include <math.h>
+using namespace std;
+struct timestamp{
+	int day;
+	int month;
+	int hh;
+	int mm;
+};
+
+struct measurement{
+	float temp;
+	float hum;
+	struct timestamp time;
+};
+
+struct city{
+	struct city *next;
+	struct measurement m;
+	int cityID;
+	int prov;
+	char city_name[50];
+};
+
+void check(struct city **head){
+	if(*head==NULL){
+		cout<<"Lista vacia."<<endl;
+		exit(1);
+	}
+}
+
+void carga_archivo(struct city **head,struct city **head2,struct city **head3){
+	struct city *nuevo=NULL;
+	struct city *aux=NULL;
+	int ID, prov, hh, mm, day, month;
+	char ciudad[50];
+	float temp, hum;
+	ifstream arch;
+	arch.open("data_set.txt", ios::in);//abrimos el  archivo en modo lectura
+	if(arch.fail()){
+		cout<<"No se pudo abrir el archivo"<<endl;
+		exit(1);
+	}
+	while(!arch.eof()){//mientras no sea el final del archivo
+		arch>>ID>>prov>>ciudad>>temp>>hum>>hh>>mm>>day>>month;
+		if(!arch.eof()){
+			if(prov==1){
+				nuevo=(struct city *)malloc(sizeof(struct city)+sizeof(struct measurement)+sizeof(struct timestamp));
+				if(nuevo==NULL){
+					cout<<"No hay memoria suficiente."<<endl;
+					exit(1);
+				}
+				nuevo->cityID=ID;
+				nuevo->prov=prov;
+				strcpy(nuevo->city_name,ciudad);
+				nuevo->m.temp=temp;
+				nuevo->m.hum=hum;
+				nuevo->m.time.hh=hh;
+				nuevo->m.time.mm=mm;
+				nuevo->m.time.day=day;
+				nuevo->m.time.month=month;
+				nuevo->next=NULL;
+				if(*head==NULL){
+					*head=nuevo;
+				}else{
+					aux=*head;
+					while(aux->next!=NULL){
+						aux=aux->next;
+					}
+					aux->next=nuevo;
+				}
+			}
+			if(prov==2){
+				nuevo=(struct city *)malloc(sizeof(struct city)+sizeof(struct measurement)+sizeof(struct timestamp));
+				if(nuevo==NULL){
+					cout<<"No hay memoria suficiente."<<endl;
+					exit(1);
+				}
+				nuevo->cityID=ID;
+				nuevo->prov=prov;
+				strcpy(nuevo->city_name,ciudad);
+				nuevo->m.temp=temp;
+				nuevo->m.hum=hum;
+				nuevo->m.time.hh=hh;
+				nuevo->m.time.mm=mm;
+				nuevo->m.time.day=day;
+				nuevo->m.time.month=month;
+				nuevo->next=NULL;
+				if(*head2==NULL){
+					*head2=nuevo;
+				}else{
+					aux=*head2;
+					while(aux->next!=NULL){
+						aux=aux->next;
+					}
+					aux->next=nuevo;
+				}
+			}
+			if(prov==3){
+				nuevo=(struct city *)malloc(sizeof(struct city)+sizeof(struct measurement)+sizeof(struct timestamp));
+				if(nuevo==NULL){
+					cout<<"No hay memoria suficiente."<<endl;
+					exit(1);
+				}
+				nuevo->cityID=ID;
+				nuevo->prov=prov;
+				strcpy(nuevo->city_name,ciudad);
+				nuevo->m.temp=temp;
+				nuevo->m.hum=hum;
+				nuevo->m.time.hh=hh;
+				nuevo->m.time.mm=mm;
+				nuevo->m.time.day=day;
+				nuevo->m.time.month=month;
+				nuevo->next=NULL;
+				if(*head3==NULL){
+					*head3=nuevo;
+				}else{
+					aux=*head3;
+					while(aux->next!=NULL){
+						aux=aux->next;
+					}
+					aux->next=nuevo;
+				}
+			}
+			
+			
+		}
+	}
+	arch.close();
+}
+
+	
+void total_muestras(struct city **head,string state){
+	struct city *aux=NULL;
+	int total=0;
+	check(head);
+	aux=*head;
+	while(aux!=NULL){
+		total++;
+		aux=aux->next;
+	}
+	cout<<"Total de "<<state<<": "<<total<<endl;
+}
+
+float calc_prom(struct city **head){
+	struct city *aux=NULL;
+	float prom=0, suma=0;
+	int c=0;
+	aux=*head;
+	check(head);
+	while(aux!=NULL){
+		suma=suma+aux->m.temp;
+		aux=aux->next;
+		c++;
+	}
+	prom=suma/c;
+	return prom;
+}
+	
+void temp_prom_prov(struct city **head,string state){
+	float prom=0;
+	prom=calc_prom(head);
+	cout<<"Temperatura promedio de "<<state<<": "<<prom<<endl;
+}
+
+
+
+
+void temp_prom_city(struct city **head){
+	struct city *actual=NULL;
+	struct city *sig=NULL;
+	float prom=0, suma=0;
+	int c=0;
+	if(*head==NULL){
+		cout<<"Lista vacia."<<endl;
+		exit(1);
+	}
+	actual=*head;
+	sig=(*head)->next;
+	while(actual!=NULL){
+		c++;
+		suma=suma+actual->m.temp;
+		if(sig==NULL || actual->cityID!=sig->cityID){
+			prom=suma/c;
+			cout<<"El promedio de la ciudad "<<actual->city_name<<"es: "<<prom<<endl;
+			c=0;
+			suma=0;
+		}
+		actual=actual->next;
+		if(actual!=NULL){
+			sig=sig->next;
+		}
+	}
+}
+void ciudad_calida(struct city **head, string state){
+	struct city *actual=NULL;
+	struct city *sig=NULL;
+	float prom=0, suma=0, mayor=0;
+	int c=0;
+	string name_city;
+	if(*head==NULL){
+		cout<<"Lista vacia."<<endl;
+		exit(1);
+	}
+	actual=*head;
+	sig=(*head)->next;
+	while(actual!=NULL){
+		c++;
+		suma=suma+actual->m.temp;
+		if(sig==NULL || actual->cityID!=sig->cityID){
+			prom=suma/c;
+			if(mayor<prom){
+				mayor=prom;
+				name_city=actual->city_name;
+			}
+			c=0;
+			suma=0;
+		}
+		actual=actual->next;
+		if(actual!=NULL){
+			sig=sig->next;
+		}
+	}
+	cout<<"La ciudad mas calidad de la provincia de "<<state<<" es "<<name_city<<" con: "<<mayor<<"°"<<endl;
+}
+void ciudad_fria(struct city **head, string state){
+	struct city *actual=NULL;
+	struct city *sig=NULL;
+	float prom=0, suma=0, menor=100;
+	int c=0;
+	string name_city;
+	if(*head==NULL){
+		cout<<"Lista vacia."<<endl;
+		exit(1);
+	}
+	actual=*head;
+	sig=(*head)->next;
+	while(actual!=NULL){
+		c++;
+		suma=suma+actual->m.temp;
+		if(sig==NULL || actual->cityID!=sig->cityID){
+			prom=suma/c;
+			if(menor>prom){
+				menor=prom;
+				name_city=actual->city_name;
+			}
+			c=0;
+			suma=0;
+		}
+		actual=actual->next;
+		if(actual!=NULL){
+			sig=sig->next;
+		}
+	}
+	cout<<"La ciudad mas fria de la provincia de "<<state<<" es "<<name_city<<" con: "<<menor<<"°"<<endl;
+}
+	
+void dia_frio(struct city **head, string state){
+	struct city *actual=NULL;
+	float menor=100;
+	int day_city=0, month_city=0;
+	if(*head==NULL){
+		cout<<"Lista vacia."<<endl;
+		exit(1);
+	}
+	actual=*head;
+	while(actual!=NULL){
+		if(actual->m.temp<menor){
+			menor=actual->m.temp;
+			day_city=actual->m.time.day;
+			month_city=actual->m.time.month;
+		}
+		actual=actual->next;
+	}
+	cout<<"La el dia mas frio de la provincia de "<<state<<" es "<<day_city<<"/"<<month_city<<" con: "<<menor<<"°"<<endl;
+}
+
+void dia_calido(struct city **head, string state){
+	struct city *actual=NULL;
+	struct city *sig=NULL;
+	float mayor=0;
+	int day_city=0, month_city=0;
+	string name_city;
+	if(*head==NULL){
+		cout<<"Lista vacia."<<endl;
+		exit(1);
+	}
+	actual=*head;
+	sig=(*head)->next;
+	while(actual!=NULL){
+		if(sig==NULL || actual->cityID!=sig->cityID){
+			if(actual->m.temp>mayor){
+				mayor=actual->m.temp;
+				day_city=actual->m.time.day;
+				name_city=actual->city_name;
+				month_city=actual->m.time.month;
+			}
+		}
+		actual=actual->next;
+		if(actual!=NULL){
+			sig=sig->next;
+		}
+	}
+	cout<<"El dia mas calido de la ciudad de "<<name_city<<" de la provincia de "<<state<<" es "<<day_city<<"/"<<month_city<<" con: "<<mayor<<"°"<<endl;
+}
+	
+void best_prov(struct city **head,struct city **head2, struct city **head3){
+	float prom_cordoba=0, prom_santaFe=0, prom_mendoza=0, cord=0, men=0, sant=0;
+	prom_cordoba=calc_prom(head);
+	prom_santaFe=calc_prom(head2);
+	prom_mendoza=calc_prom(head3);
+	cord=fabs(prom_cordoba-23.0);
+	sant=fabs(prom_santaFe-23.0);
+	men=fabs(prom_mendoza-23.0);
+	if(cord<sant && cord<men){
+		cout<<"Cordoba es la mejor provincia para el cultivos de pimientos"<<endl;
+	}
+	if(sant<cord && sant<men){
+		cout<<"Santa Fe es la mejor provincia para el cultivos de pimientos"<<endl;
+	}
+	if(men<cord && men<sant){
+		cout<<"Mendoza es la mejor provincia para el cultivos de pimientos"<<endl;
+	}
+	
+}
---
+++ Main_principal.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <fstream>
+#include "Funciones.h"
+
+using namespace std;
+
+void menu(void);
+
+
+int main(int argc, char *argv[]) {
+	struct city *head=NULL; 
+	struct city *head2=NULL;
+	struct city *head3=NULL;
+	char op=' ';
+	carga_archivo(&head,&head2,&head3);
+	do{
+		menu();
+		cin>>op;
+		switch(op){
+		case 'a': 
+		case 'A':
+			cout<<"-----------------------------------------------------------"<<endl;
+			total_muestras(&head,"Cordoba");
+			total_muestras(&head2,"Santa Fe");
+			total_muestras(&head3,"Mendoza");
+			cout<<"-----------------------------------------------------------"<<endl;
+			break;
+		case 'b': 
+		case 'B':
+			cout<<"-----------------------------------------------------------"<<endl;
+			temp_prom_prov(&head,"Cordoba");
+			temp_prom_prov(&head2,"Santa Fe");
+			temp_prom_prov(&head3,"Mendoza");
+			cout<<"-----------------------------------------------------------"<<endl;
+			break;
+		case 'c': 
+		case 'C':
+			cout<<"-----------------------------------------------------------"<<endl;
+			cout<<"Cordoba:"<<endl;
+			temp_prom_city(&head);
+			cout<<"Santa Fe:"<<endl;
+			temp_prom_city(&head2);
+			cout<<"Mendoza:"<<endl;
+			temp_prom_city(&head3);
+			cout<<"-----------------------------------------------------------"<<endl;
+			break;
+		case 'd': 
+		case 'D':
+			ciudad_calida(&head,"Cordoba");
+			ciudad_calida(&head2,"Santa Fe");
+			ciudad_calida(&head3,"Mendoza");
+			break;
+		case 'e': 
+		case 'E':
+			ciudad_fria(&head,"Cordoba");
+			ciudad_fria(&head2,"Santa Fe");
+			ciudad_fria(&head3,"Mendoza");
+			break;
+		case 'f': 
+		case 'F':
+			dia_frio(&head,"Cordoba");
+			dia_frio(&head2,"Santa Fe");
+			dia_frio(&head3,"Mendoza");
+			break;
+		case 'g': 
+		case 'G':
+			dia_calido(&head,"Cordoba");
+			dia_calido(&head2,"Santa Fe");
+			dia_calido(&head3, "Mendoza");
+			break;
+		case 'h': 
+		case 'H':
+			best_prov(&head,&head2,&head3);
+			break;
+		}
+		cout<<"¿Deseas continuar(s/n)?:"<<endl;
+		cin>>op;
+	} while(op=='s'||op=='S');
+	
+	
+	
+	return 0;
+}
+
+void menu(void){
+	cout<<"--------------------------------------------------------------------"<<endl;
+	cout<<"Elige una opcion:"<<endl;
+	cout<<"a-Total de las muestras almacenadas en las listas pertenecientes a cada provincia."<<endl;
+	cout<<"b-Temperatura promedio de cada provincia."<<endl;
+	cout<<"c-Temperatura promedio de cada ciudad."<<endl;
+	cout<<"d-Ciudad mas calida de cada provincia."<<endl;
+	cout<<"e-Ciudad mas fria de cada provincia."<<endl;
+	cout<<"f-Dia mas frio de cada provincia."<<endl;
+	cout<<"g-Dia mas calido de cada ciudad."<<endl;
+	cout<<"h-Mejor provincia para el cultivo de pimientos."<<endl;
+	cout<<"--------------------------------------------------------------------"<<endl;
+}
+
+
+
+
